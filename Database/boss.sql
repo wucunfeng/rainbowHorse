b502af51a47d926f2916e7f09a94699dd1cb240b
Cola <wu1998102@gmail.com>
2019-01-26 21:15:09 +0800

    这次数据可以存入

--- Database/boss.sql
+++ Database/boss.sql
@@ -1,13 +1,11 @@
 CREATE TABLE Boss
 (
-    jobTitle    CHAR (20)NOT NULL, -- 岗位
-    lowSalary   TINYINT (10)NOT NULL, -- 最低工资
-    highSalary  TINYINT (10)NOT NULL, -- 最高工资
-    jobSite     CHAR (30)NOT NULL,  -- 上班地点
-    jobExperience CHAR (10)NOT NULL, -- 工作经验
-    jobFinancing CHAR (5)NOT NULL, -- 学历
-    jobEmployees VARCHAR (1000)NOT NULL, -- 公司人数
-    jobRelease   DATE NOT NULL, -- 发布日期
-    PRIMARY KEY (jobTitle)
+    jobTitle    CHAR (20) NOT NULL, -- 岗位
+    lowSalary   CHAR (10) NULL, -- 最低工资
+    highSalary  CHAR (10) NULL, -- 最高工资
+    jobSite     CHAR (30) NULL,  -- 上班地点
+    jobExperience CHAR (10) NULL, -- 工作经验
+    jobFinancing CHAR (5) NULL, -- 学历
+    jobEmployees VARCHAR (1000) NULL, -- 公司人数
+    jobRelease   DATE NULL, -- 发布日期
 );
-
